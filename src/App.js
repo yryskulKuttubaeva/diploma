7cc0d0e746db69cadc111ec12d667f1c1b9f850e
yryskul <kuttubaevayryskul@gmail.com>
2022-04-11 15:52:02 +0600

    inport everything

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import Nav from "./components/Nav/Nav"
-import Header from "./components/Header/Header"
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
 
-function App (){
-    return (
-        <div className="App">
-         <Nav/>
-         <Header/>
-         <Footer/>
+import Home from "./pages/Home";
+import Glass from "./pages/Glass";
+import Solar from "./pages/Solar";
+import Minus from "./pages/Minus";
+import Pilus from "./pages/Pilus";
+import { Route, Routes } from "react-router-dom";
 
-        </div>
-    )
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/glass" element={<Glass />} />
+        <Route path="/solar" element={<Solar />} />
+        <Route path="/minus" element={<Minus />} />
+        <Route path="/pilus" element={<Pilus />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
 }
+
 export default App;
