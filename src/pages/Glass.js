fd7a43cc3a418a7303bae05753a7220c9e439058
yryskul <kuttubaevayryskul@gmail.com>
2022-04-11 15:56:33 +0600

    add artile, h1, p

---
+++ src/pages/Glass.js
@@ -0,0 +1,10 @@
+function Glass() {
+    return (
+      <article>
+        <h1>This is glass page.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex pariatur voluptatum doloribus enim repudiandae ullam sequi quo suscipit voluptas id, laboriosam necessitatibus, dolore corrupti. Explicabo iure doloribus illum repudiandae tenetur.</p>
+      </article>
+    );
+  }
+  
+  export default Glass;
