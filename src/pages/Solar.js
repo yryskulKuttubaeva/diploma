d2d9fa02bf32aa287463e2fddabbd72a732f7993
yryskul <kuttubaevayryskul@gmail.com>
2022-04-11 15:57:00 +0600

    add artile, h1, p

---
+++ src/pages/Solar.js
@@ -0,0 +1,10 @@
+function Solar() {
+    return (
+      <article>
+        <h1>This is solar page.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex pariatur voluptatum doloribus enim repudiandae ullam sequi quo suscipit voluptas id, laboriosam necessitatibus, dolore corrupti. Explicabo iure doloribus illum repudiandae tenetur.</p>
+      </article>
+    );
+  }
+  
+  export default Solar;
