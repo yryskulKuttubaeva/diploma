45c0e69778f959ec1390cc6ab3a69c859bffface
yryskul <kuttubaevayryskul@gmail.com>
2022-04-11 15:56:47 +0600

    add artile, h1, p

---
+++ src/pages/Minus.js
@@ -0,0 +1,10 @@
+function Minus() {
+    return (
+      <article>
+        <h1>This is minus page.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex pariatur voluptatum doloribus enim repudiandae ullam sequi quo suscipit voluptas id, laboriosam necessitatibus, dolore corrupti. Explicabo iure doloribus illum repudiandae tenetur.</p>
+      </article>
+    );
+  }
+  
+  export default Minus;
