1bdec3ad5f10cedb4b60217abf63196efebcf675
yryskul <kuttubaevayryskul@gmail.com>
2022-04-13 15:12:17 +0600

    change text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,17 @@
 import "./Header.css";
 import headerImage from "../../assets/header.jpg";
 
+
 function Header() {
   const style = {
     backgroundImage: `url(${headerImage})`
+  
   };
 
   return (
     <header className="Header" style={style}>
       <h1>I see perfectly</h1>
-      <h4 >You want to see great too.</h4> 
+      <h3 > Welcome to our optics shop</h3> 
     
      
 
