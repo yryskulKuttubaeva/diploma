034fef39d3e08fae60c1f04799872600edf80eba
yryskul <kuttubaevayryskul@gmail.com>
2022-04-13 14:28:26 +0600

    change plus

--- src/pages/Pilus.js
+++ src/pages/Pilus.js
@@ -1,7 +1,7 @@
 function Pilus() {
     return (
       <article>
-        <h1>This is pilus page.</h1>
+        <h1>This is plus page.</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex pariatur voluptatum doloribus enim repudiandae ullam sequi quo suscipit voluptas id, laboriosam necessitatibus, dolore corrupti. Explicabo iure doloribus illum repudiandae tenetur.</p>
       </article>
     );
