330dfc06320d0829ca53018edf1284b49db12115
yryskul <kuttubaevayryskul@gmail.com>
2022-04-14 19:31:57 +0600

    add Plus

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,15 @@ import headerImage from "../../assets/header.jpg";
 
 function Header() {
   const style = {
-    backgroundImage: `url(${headerImage})`
+    backgroundImage: `url(${headerImage})`, 
+
   
   };
 
   return (
     <header className="Header" style={style}>
       <h1>I see perfectly</h1>
-      <h3 > Welcome to our optics shop</h3> 
+      <h4> Welcome to our optics shop</h4> 
     
      
 
