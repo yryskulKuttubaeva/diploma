392795cd3d8f342b34744fe3eca3b8a9ed7f57f3
yryskul <kuttubaevayryskul@gmail.com>
2022-04-20 12:46:14 +0600

    added Layout

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Nav from "./components/Nav/Nav";
-
 import Home from "./pages/Home";
 import Glass from "./pages/Glass";
 import Solar from "./pages/Solar";
 import Minus from "./pages/Minus";
 import Plus from "./pages/Plus";
 import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Header />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/minus" element={<Minus />} />
-        <Route path="/plus" element={<Plus />} />
-        <Route path="/glass" element={<Glass />} />
-        <Route path="/solar" element={<Solar />} />
-      </Routes>
-
-      <Footer />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/minus" element={<Minus />} />
+          <Route path="/plus" element={<Plus />} />
+          <Route path="/glass" element={<Glass />} />
+          <Route path="/solar" element={<Solar />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
