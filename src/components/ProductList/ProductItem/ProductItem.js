3baa6b1ac0be8c454cfec9d59da6b3e006e45069
yryskul <kuttubaevayryskul@gmail.com>
2022-04-22 15:20:15 +0600

    corrected alts problems in netlify

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -4,7 +4,7 @@ import classes from "./GlassItem.module.css";
 function GlassItem({ glass }) {
   return (
     <div className={classes.GlassItem}>
-      <img src={glass.image} />
+      <img src={glass.image} alt="img" />
       <Link to={glass.path}>{glass.title}</Link>
       <div className={classes.price}>${glass.price}</div>
     </div>
